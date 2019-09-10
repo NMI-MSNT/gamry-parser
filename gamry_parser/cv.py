0d93d0017f941f021af3a3de96c59a5bd1f80bbc
Peter Jones <peter.jones@nmi.de>
2019-09-10 09:08:51 +0200

    fixed conflict

--- gamry_parser/cv.py
+++ gamry_parser/cv.py
@@ -58,7 +58,7 @@ class CyclicVoltammetry(parser.GamryParser):
         assert curve >= 0, 'Invalid curve ({}). Indexing starts at 0'.format(curve)
         assert curve < self.curve_count, 'Invalid curve ({}). File contains {} total curves.'.format(curve, self.curve_count)
         df = self.curves[curve]
-        
+
         return df[['Vf', 'Im']]
 
     def get_peaks(self, curve=0, width_V=0.02, peak_range_x=None):
